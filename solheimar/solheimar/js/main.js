bfc8509194d5eb97ab2a07a27aae4172f95c1192
Jacob Kombedjian <jkombedjian@gmail.com>
2025-10-30 14:26:31 -0600

    Delete solheimar directory

--- solheimar/solheimar/js/main.js
+++
@@ -1,7 +0,0 @@
-var btn = document.querySelector('.toggle-btn');
-
-btn.addEventListener('click', function(){
-    document.querySelector('nav').classList.toggle('show-nav');
-    let expanded = this.getAttribute('aria-expanded') === 'true' || false;
-    this.setAttribute('aria-expanded', !expanded);
-});
