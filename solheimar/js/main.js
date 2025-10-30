0c1e02a801d576816983521732c80d1e2ef9e4d2
Jacob Kombedjian <jkombedjian@gmail.com>
2025-10-30 14:27:18 -0600

    Add files via upload

---
+++ solheimar/js/main.js
@@ -0,0 +1,7 @@
+var btn = document.querySelector('.toggle-btn');
+
+btn.addEventListener('click', function(){
+    document.querySelector('nav').classList.toggle('show-nav');
+    let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+});
